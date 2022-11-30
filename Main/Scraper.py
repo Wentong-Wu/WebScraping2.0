5f9b00f0da70db9bf85be1c2d0e7d18d7bb0989b
Tom Wu <80329793+Wentong-Wu@users.noreply.github.com>
2022-11-30 18:30:59 +0000

    Added methods to scraper class

--- Main/Scraper.py
+++ Main/Scraper.py
@@ -26,9 +26,41 @@ class Scraper:
         nav_bar = self.driver.find_element(By.XPATH, '//a[@data-content="'+data_content+'"]')
         nav_bar.click()
 
+    def scroll_to_end(self):
+        """
+        Scrolls down to the end of the page.
+        """
+        #Scroll all the way down to get all the products
+        length_of_page = self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight);var length_of_page=document.body.scrollHeight;return length_of_page;")
+        page_end = False
+        while(page_end==False):
+            last_length_page = length_of_page
+            time.sleep(1)
+            length_of_page = self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight);var length_of_page=document.body.scrollHeight;return length_of_page;")
+            if last_length_page==length_of_page:
+                try:
+                    self.driver.find_element(By.XPATH,'//*[@class="infinite-load"]').find_element(By.XPATH,'.//button').click()
+                except:
+                    page_end=True
+                    
+    def get_all_element_links(self, xpath: str = '//*[@class="search-results-list"]'):
+        books = self.driver.find_element(By.XPATH,xpath)
+        list_of_books = books.find_elements(By.XPATH,'.//*[@class="image-wrap"]/a')
+        link_of_books = []
+        for book in list_of_books:
+            link_of_books.append(book.get_attribute("href"))
+        return link_of_books
+    
+    def open_links(self, links):
+        for link in links:
+            self.driver.get(link)
+        pass
+
 if __name__ == "__main__":
     website = Scraper("https://www.waterstones.com/")
     website.accept_cookies()
     nav_name = ["NEW","COMING SOON","SPECIAL EDITIONS"]
     website.get_navigation_bar(nav_name[1])
+    website.scroll_to_end()
+    website.open_links(website.get_all_element_links())
     pass
